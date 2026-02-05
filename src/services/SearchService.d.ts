e4f2c9a9d34b189d55145aaf9a1dd47b570ec483
gisotra <gisotra.henrique@gmail.com>
2026-02-05 18:40:01 -0300

    ts.config update: Agora ele não polui mais o projeto com arquivos .d.ts e d.ts.map, que são usados para bibliotecas + public -> dist

--- src/services/SearchService.d.ts
+++
@@ -1,6 +0,0 @@
-import { GithubUser } from "../models/User";
-export declare class SearchService {
-    private apiURL;
-    getUser(username: string): Promise<GithubUser | null>;
-}
-//# sourceMappingURL=SearchService.d.ts.map
